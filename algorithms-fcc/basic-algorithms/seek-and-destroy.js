ae62b7957a29630b64ebfe2fb2d3b3dbab9d8f60
Joshmurph316 <Joshmurph316@gmail.com>
2017-08-04 09:51:29 -0400

    completed rotation day 0

--- algorithms-fcc/basic-algorithms/seek-and-destroy.js
+++ algorithms-fcc/basic-algorithms/seek-and-destroy.js
@@ -1,5 +1,17 @@
-module.exports = function destroyer(arr) {
+// *****Seek and destroy function*****
 
+// remove all array elements that match additional arguments
+module.exports = function destroy(arr) {
+
+    // loop thru and collect args
+    var argsArray = [];
+
+    for (var i = 1; i < arguments.length; i++) {
+        argsArray.push(arguments[i]);
+    }
+    for (var x = 0; x < argsArray.length; x++) {
+        // remove elements from initial array
+        arr = arr.filter(num => num !== argsArray[x]);
+    }
+    return arr + " removed " + argsArray;
 };
-
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
---
+++ algorithms-fcc/index.js
@@ -0,0 +1,4 @@
+const destroy = require('./basic-algorithms/seek-and-destroy');
+
+console.log(destroy([1, 2, 3, 1, 2, 3], 2, 3));
+console.log(destroy(["tree", "hamburger", 53], "tree", 53));
